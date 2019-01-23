619fdff9bba16e1f74d4391210205fc2d4c24c84
Bodhish Thomas <bodhish@gmail.com>
2019-01-24 01:05:48 +0530

    Add basic structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Villa from './villa';
+
+class App extends Component {
+  render() {
+    return (
+      <Villa></Villa>
+    );
+  }
+}
+
+export default App;
