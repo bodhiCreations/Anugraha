686e3809f6c50d41d8e9ab60622de5de4cf72003
Bodhish Thomas <bodhish@gmail.com>
2019-01-25 03:23:44 +0530

    Updates

---
+++ precache-manifest.0cc5bf980fd8d0d8629bc7d9e0f60c54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24bfc4d5a4b9e8b2c6a7",
+    "url": "/static/js/main.24bfc4d5.chunk.js"
+  },
+  {
+    "revision": "066a4a6485735ddbdd19",
+    "url": "/static/js/1.066a4a64.chunk.js"
+  },
+  {
+    "revision": "24bfc4d5a4b9e8b2c6a7",
+    "url": "/static/css/main.5a856b79.chunk.css"
+  },
+  {
+    "revision": "2ab6a2fe6bbf5cc78bf893ebc4a20ac5",
+    "url": "/index.html"
+  }
+];
